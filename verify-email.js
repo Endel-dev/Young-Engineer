95ee3a39b5e8279c964722daa4b5dd85015446ed
Endel-dev <akshay.dhruv@endel.digital>
2024-12-20 16:14:24 +0530

    create-guardian API Updated

--- verify-email.js
+++ verify-email.js
@@ -3,37 +3,35 @@ async function verifyEmail() {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
     const email = urlParams.get('email');
-
+  
     if (!token || !email) {
-        alert('Missing token or email.');
-        return;
+      alert('Missing token or email.');
+      return;
     }
-
+  
     try {
-        const response = await fetch('http://93.127.172.167:5001/verify-email1', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ token, email }),
-        });
-        const data = await response.json();
-
-        //if (response.ok) {
-        //alert(data.message);
+      const response = await fetch('http://93.127.172.167:5001/verify-email1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token, email }),
+      });
+      const data = await response.json();
+  
+      if (response.ok) {
+        alert(data.message);
         //window.location.href = data.redirectUrl;
-        //window.location.href='http://93.127.172.167:5001/login'; // Redirect to login page
-        if (response.ok && data.status === 1) {
-            alert(data.message);  // Success message
-            window.location.href = '/login';  // Redirect to the login page
-        } else {
-            alert(data.message);
-        }
+         window.location.href='http://93.127.172.167:5001/login'; // Redirect to login page
+      } else {
+        alert(data.message);
+      }
     } catch (error) {
-        alert('An error occurred. Please try again later.');
-        console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+      console.error('Error:', error);
     }
-}
-
-// Call verifyEmail function when the page loads
-//document.addEventListener('DOMContentLoaded', verifyEmail);
+  }
+  
+  // Call verifyEmail function when the page loads
+  //document.addEventListener('DOMContentLoaded', verifyEmail);
+  
