7ebe56dcf630c5f25a297c2ba6f78538b3664ed7
Endel-dev <akshay.dhruv@endel.digital>
2025-01-04 16:26:48 +0530

    Register API Updated

--- models/User.js
+++ models/User.js
@@ -104,37 +104,38 @@ userSchema.pre("save", function (next) {
 });
 
 // Hash password before saving (bcryptjs for hashing)
-userSchema.pre("save", async function (next) {
-    if (typeof this.dob === 'string') {
-      const dobParts = this.dob.split('-');
-      if (dobParts.length === 3) {
-        const day = dobParts[0];
-        const month = dobParts[1] - 1; // Month is 0-indexed in JavaScript Date
-        const year = dobParts[2];
+// userSchema.pre("save", async function (next) {
+//     if (typeof this.dob === 'string') {
+//       const dobParts = this.dob.split('-');
+//       if (dobParts.length === 3) {
+//         const day = dobParts[0];
+//         const month = dobParts[1] - 1; // Month is 0-indexed in JavaScript Date
+//         const year = dobParts[2];
   
-        // Create a Date object from the string
-        this.dob = new Date(year, month, day);
+//         // Create a Date object from the string
+//         this.dob = new Date(year, month, day);
   
-        if (isNaN(this.dob)) {
-          return next(new Error("Invalid date format. Expected dd-mm-yyyy."));
-        }
-      } else {
-        return next(new Error("Invalid date format. Expected dd-mm-yyyy."));
-      }
-    }
+//         if (isNaN(this.dob)) {
+//           return next(new Error("Invalid date format. Expected dd-mm-yyyy."));
+//         }
+//       } else {
+//         return next(new Error("Invalid date format. Expected dd-mm-yyyy."));
+//       }
+//     }
+
 
     
-  if (!this.isModified("password")) return next();
+//   if (!this.isModified("password")) return next();
 
-  // Only hash if password is modified
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+//   // Only hash if password is modified
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 
 
