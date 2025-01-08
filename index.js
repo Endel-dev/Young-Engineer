1e69e6d38cac942cf2fc975da1565f67bc104f43
Endel-dev <akshay.dhruv@endel.digital>
2025-01-08 16:01:18 +0530

    Parent Update task Status API

--- index.js
+++ index.js
@@ -3554,7 +3554,7 @@ app.post("/transfer-fair-amount", async (req, res) => {
 
     // 3. Ensure that the childUserId exists in the family
     const child = await User.findOne({ userId: childUserId, familyId: familyId ,taskStatus: "completed",  // Ensure task is completed
-      paymentStatus: "pending", })
+       })
     if (!child) {
       return res.status(400).json({ status: 0, message: "Child not found in this family" });
     }
